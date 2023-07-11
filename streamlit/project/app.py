cf01c90c68e28c59f218eaeaa4809c6e8e4f0c22
Martin Erpicum <martin@erpicum.net>
2023-07-11 17:12:51 +0200

    trying to generate svg with svgwrite

--- streamlit/project/app.py
+++ streamlit/project/app.py
@@ -6,9 +6,24 @@ import main_lib as lib
 st.set_page_config(page_title="Metronome")
 st.title("Metronome")
 
+svg_code = """"
+<svg xmlns="http://www.w3.org/2000/svg" width="200" height="200">
+  <circle cx="100" cy="100" r="50" fill="black" /> <!-- Rond noir -->
+  <line x1="100" y1="100" x2="100" y2="100" id="line" stroke="black" stroke-width="2" /> <!-- Ligne horizontale initiale -->
+  <animate xlink:href="#line" attributeName="x2" dur="1s" from="100" to="150" fill="freeze" /> <!-- Animation de la ligne vers la droite -->
+  <circle cx="150" cy="100" r="5" fill="black" /> <!-- Cercle d'arrivée -->
+</svg>
+"""
+
+
 text = "Welcome to the first day... of the rest... of your life"
+
+st.markdown(svg_code, unsafe_allow_html=True)
 
 t = st.empty()
 for i in range(len(text) + 1):
     t.markdown("## %s..." % text[0:i])
     time.sleep(0.1)
+
+
+
--- streamlit/project/main_lib.py
+++ streamlit/project/main_lib.py
@@ -1,2 +1,38 @@
 import os
 import glob
+
+
+import svgwrite
+
+# Création du dessin SVG avec une taille de 500x200
+dwg = svgwrite.Drawing(filename='output.svg', size=('500px', '200px'))
+
+# Définition de la ligne principale
+line = dwg.line((50, 100), (350, 100), stroke='black', stroke_width=2)
+dwg.add(line)
+
+# Ajout des cercles
+circle_count = 6  # Nombre de cercles à ajouter
+circle_spacing = 100  # Espacement horizontal entre les cercles
+circle_radius = 20  # Rayon des cercles
+circle_fill = 'white'  # Couleur de remplissage des cercles
+circle_stroke = 'black'  # Couleur de la bordure des cercles
+circle_stroke_width = 2  # Épaisseur de la bordure des cercles
+
+for i in range(circle_count):
+    cx = 50 + i * circle_spacing
+    circle = dwg.circle(center=(cx, 100), r=circle_radius, fill=circle_fill,
+                        stroke=circle_stroke, stroke_width=circle_stroke_width)
+    dwg.add(circle)
+
+    # Ajouter une animation de changement de couleur pour chaque cercle
+    animation_dur = str(0.5 + i * 0.25) + 's'  # Durée de l'animation en fonction de l'indice du cercle
+    animation_begin = str(i * 0.5) + 's'  # Début de l'animation en fonction de l'indice du cercle
+    animation_values = ';'.join([circle_fill, 'red', circle_fill])  # Valeurs de couleur pour l'animation
+
+    animate = dwg.animate(attributeName='fill', dur=animation_dur, values=animation_values,
+                          begin=animation_begin, repeatCount='indefinite')
+    circle.add(animate)
+
+# Enregistrer le dessin SVG dans un fichier
+dwg.save()
